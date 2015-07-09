8c218ca4beb37b86d400198bf6d491257c27eb3a
Erik Hedberg <erik@gish.se>
2015-07-09 11:40:46 +0200

    Read database name from env var

--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ config = {
         port: process.env.PORT || 80,
         database: {
             type: 'mysql',
-            database: 'transactions',
+            database: process.env.DB_NAME,
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD
         }
