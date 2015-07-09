600a0627172e3daf0e39b66385bc7c98b3da616b
Erik Hedberg <erik@gish.se>
2015-07-09 21:37:43 +0200

    Set env var before running migration

--- install.js
+++ install.js
@@ -1,14 +1,22 @@
-var connection = require('./server/services/mysql-connector.js'),
-    migrations = require('./server/migrations.js'),
+var migrations = require('./server/migrations.js'),
     lastMigration = parseInt(process.env.DB_MIGRATION_LAST, 10) || 0,
-    _ = require('lodash');
+    _ = require('lodash'),
+    config = require('./server/config.js'),
+    connection,
+    environment;
+
+
+environment = process.env.NODE_ENV || 'development';
+config(environment);
+
+connection = require('./server/services/mysql-connector.js');
 
 _.map(migrations, function(migration) {
     if (migration.timestamp > lastMigration) {
         console.log("Running migration: ", migration.description);
         connection.query(migration.statement, function(err, results) {
             if (err) {
-                console.error('Migration failed', err);
+                throw new Error('Migration ' + migration.description + ' failed');
             } else {
                 lastMigration = migration.timestamp;
             }
