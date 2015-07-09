34ed12ec40399d70be872bc546d099d09e4d9c8b
Erik Hedberg <erik@gish.se>
2015-07-09 11:24:01 +0200

    Add list, add, delete for mysql db

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -1,17 +1,33 @@
-var connection = require('./../libs/mysql-connector.js'),
+var mysql = require('./../services/mysql-connector.js'),
     transactions,
-    _save;
+    queryCb;
 
-_save = function() {
+queryCb = function(cb) {
+    return function(err, data) {
+        if (err) {
+            console.error(err);
+            throw new Error('mySQL error');
+        } else {
+            if (cb) {
+                cb(data);
+            }
+        }
+    };
 };
 
 module.exports = {
     init: function(options) {
     },
     load: function(cb) {
+        var query = 'SELECT id, sender, receiver, amount, UNIX_TIMESTAMP(date) as date FROM transactions';
+        mysql.query(query, queryCb(cb));
     },
-    add: function(data) {
+    add: function(data, cb) {
+        var query = 'INSERT INTO transactions(id, sender, receiver, amount, date) VALUES(?, ?, ?, ?, FROM_UNIXTIME(?))';
+        mysql.query(query, [data.id, data.sender, data.receiver, data.amount, data.date], queryCb(cb));
     },
-    delete: function(id) {
+    delete: function(id, cb) {
+        var query = 'DELETE FROM transactions where id = ?';
+        mysql.query(query, [id], queryCb(cb));
     }
 };
