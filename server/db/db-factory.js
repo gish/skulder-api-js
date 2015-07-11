6c42d97c8fe26a6e975fe9a4d30b4ef11beb1527
Erik Hedberg <erik@gish.se>
2015-07-11 19:05:11 +0200

    Use mysql as dev db

--- server/db/db-factory.js
+++ server/db/db-factory.js
@@ -1,12 +1,6 @@
 var factory;
 
 factory = function(options) {
-    if (options.type === 'fixtures') {
-        var fixtureDb = require('./fixture.js');
-        fixtureDb.init(options);
-        return fixtureDb;
-    }
-
    if (options.type === 'mysql') {
         var mysqlDb = require('./mysql.js');
         mysqlDb.init(options);
